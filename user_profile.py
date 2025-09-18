e516cf8002411819c449f49bed97b1ab200f87ad
Your Name <mjshawa23@gmail.com>
2025-09-19 00:09:28 +0200

    Day 7: python input and formated output

---
+++ user_profile.py
@@ -0,0 +1,14 @@
+#day-7-task python-project
+#user_profile.py
+#getting user's input
+
+name = input("Hello, enter your name: ")
+age = input("Enter your age: ")
+favouritePLanguage = input("Enter your favourite programming language: ")
+hobby = input("Enter your favourite hobby: ")
+
+#printed format, output summary
+
+print(f"Hello  {name}")
+print(f"You are: {age} years old.")
+print(f"Your favourite programing language is {favouritePLanguage} and Your favourite hobby is {hobby}.")
